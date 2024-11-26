ecfe32bb9daffbbb1e78037a627828636a30f76d
grigoreva.darya13 <grigoreva.darya13@wildberries.work>
2024-11-26 18:30:10 +0400

    fix url

--- crud-front/src/api/api.js
+++ crud-front/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/items';
+const API_URL = 'https://todo-app-js-4hjn.onrender.com/api/items';
 
 // Получение списка элементов
 export async function fetchItems() {
