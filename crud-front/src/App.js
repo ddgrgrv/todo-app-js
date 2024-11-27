7cf27548a4a5011739f61f4fd96b0b680d309a44
grigoreva.darya13 <grigoreva.darya13@wildberries.work>
2024-11-27 15:55:22 +0400

    update: App.js

--- crud-front/src/App.js
+++ crud-front/src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/crud-front">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
