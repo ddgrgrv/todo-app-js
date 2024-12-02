514e166d92b425f17795bc1b91d46729df18a53f
grigoreva.darya13 <grigoreva.darya13@wildberries.work>
2024-12-02 20:44:30 +0400

    fix: styles

--- crud-front/src/index.js
+++ crud-front/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/home.css';
+import './styles/root.css';
 import { AuthContextProvider } from './context/auth-context'; // Провайдер контекста
 
 const rootElement = document.getElementById('root');
