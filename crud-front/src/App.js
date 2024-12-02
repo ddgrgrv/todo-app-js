0a0e19efeecdcbc91117715202a08b38be5d1a46
grigoreva.darya13 <grigoreva.darya13@wildberries.work>
2024-12-02 20:35:24 +0400

    fix: routes

--- crud-front/src/App.js
+++ crud-front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import LoginPage from './components/LoginForm';
@@ -7,14 +7,14 @@ import RegisterPage from './components/SignUpForm';
 
 function App() {
   return (
-    <BrowserRouter basename="/todo-app-js/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
