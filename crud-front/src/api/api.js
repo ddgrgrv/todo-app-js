d8b51f869929f52c22862f04aa22bb9e8293bd2b
grigoreva.darya13 <grigoreva.darya13@wildberries.work>
2024-12-02 20:20:55 +0400

    update: bundle

--- crud-front/src/api/api.js
+++ crud-front/src/api/api.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://todo-app-js-4hjn.onrender.com/api/items';
-const API_URL = 'http://localhost:3000/api/items';
+const API_URL = 'https://todo-app-js-4hjn.onrender.com/api/items';
+// const API_URL = 'http://localhost:3000/api/items';
 
 
 // Получение списка элементов
